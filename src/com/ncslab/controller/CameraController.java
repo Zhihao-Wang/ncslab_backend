b01cd976ce458503191c391d674267ae884f5edb
RocketWong <wzh07@hotmail.com>
2017-12-17 21:05:08 +0800

    ncs

--- src/com/ncslab/controller/CameraController.java
+++ src/com/ncslab/controller/CameraController.java
@@ -4,20 +4,17 @@ package com.how2java.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import java.util.Map;
 
 import com.alibaba.fastjson.JSONObject;
 import com.how2java.pojo.Camera;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
 import com.how2java.service.CameraService;
 
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 // 告诉spring mvc这是一个控制器类
